79334b193765b53f656e1b2f06bf9e574e16dc14
Enwọñọ Ntuk <enwonontuk20@gmail.com>
2026-04-30 17:06:01 +0100

    drop dead weight

--- add-unique-nickname.sql
+++
@@ -1,2 +0,0 @@
--- Run this in your Supabase SQL Editor to ensure nicknames are unique
-ALTER TABLE members ADD CONSTRAINT members_nick_name_key UNIQUE (nick_name);
--- add_spotify_column.sql
+++
@@ -1,2 +0,0 @@
--- Run this in your Supabase SQL Editor to support the Spotify feature
-ALTER TABLE members ADD COLUMN IF NOT EXISTS favorite_song JSONB DEFAULT NULL;
